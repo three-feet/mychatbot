36d24f8474489af28feaee161e01d7838c33cca9
three-feet <hoony8111@hanmail.net>
2026-05-27 17:34:22 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -21,9 +21,9 @@ from openai import AsyncOpenAI
 # =========================
 load_dotenv()
 
-AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+AZURE_OPENAI_ENDPOINT = st.secrets["AZURE_OPENAI_ENDPOINT"]
+AZURE_OPENAI_API_KEY = st.secrets["AZURE_OPENAI_API_KEY"]
+AZURE_OPENAI_DEPLOYMENT = st.secrets["AZURE_OPENAI_DEPLOYMENT"]
 
 # =========================
 # Streamlit 설정
